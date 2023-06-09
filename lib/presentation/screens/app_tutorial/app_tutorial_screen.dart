c0d95ab09ae5cc2658a2b842e028e3c78a1bd2ba
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-06-09 16:02:16 -0500

    screens created

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+
+  static const name = 'app_tutorial_scren';
+  
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
