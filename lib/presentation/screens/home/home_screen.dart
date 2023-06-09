b954b159c64091208961ab35e764cdf2414858b4
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-06-09 12:17:01 -0500

    routes name implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+  
   const HomeScreen({super.key});
 
   @override
@@ -54,6 +58,7 @@ class _CustomListTitle extends StatelessWidget {
       subtitle:  Text( menuItem.subTitle ),
       onTap: () {
         // Navigator
+        // context.pushNamed( CardsScreen.name );
         context.push( menuItem.link );
       },
     );
